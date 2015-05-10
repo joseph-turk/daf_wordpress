016bfe9ab6956899ef6aba084b5d44a3447efe1a
Joseph Turk <joseph.r.turk@gmail.com>
2015-05-10 10:58:36 -0400

    Updated article view to include hero image

--- wp-content/themes/daf/single.php
+++ wp-content/themes/daf/single.php
@@ -15,25 +15,31 @@
 
 <?php /* If the post COULD be found */ ?>
 <?php while ( have_posts() ) : the_post(); ?>
-  <div class="row">
-    <div class="small-12 columns">
-      <h2><?php the_title(); ?></h2>
-    </div>
+  <div class="hero">
+    <img src="<?php the_field('hero_image'); ?>">
   </div>
 
-  <div class="row">
-    <div class="small-12 columns">
-      <?php the_content(); ?>
+  <article>
+    <div class="row">
+      <div class="small-12 columns">
+        <h2><?php the_title(); ?></h2>
+      </div>
     </div>
-  </div>
 
-  <div class="row">
-    <div class="small-12 columns">
-      <p class="date-and-author">
-        posted <?php the_date(); ?> by <?php the_author(); ?>
-      </p>
+    <div class="row">
+      <div class="small-12 columns">
+        <?php the_content(); ?>
+      </div>
     </div>
-  </div>
+
+    <div class="row">
+      <div class="small-12 columns">
+        <p class="date-and-author">
+          posted <?php the_date(); ?> by <?php the_author(); ?>
+        </p>
+      </div>
+    </div>
+  </article>
 <?php endwhile; ?>
 
 <?php get_footer(); ?>
